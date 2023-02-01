edc30bd66570352554361e47d92506b909c059c0
JoMaSole <105064348+JoMaSole@users.noreply.github.com>
2023-02-01 17:33:11 -0300

    inicios

---
+++ 01_print.py
@@ -0,0 +1,22 @@
+#se concatenan las variables con una , (coma)
+
+my_name = "Joan"
+my_age = 32
+print ("Hola mi nombre es ", my_name , " y tengo " , my_age , " años.")
+
+#concatenar completo
+my_apellido = "Sole"
+completo = my_name + " " + my_apellido
+print(completo)
+
+
+#input
+#genera el input y se queda a la espera de que ingreses un valor para la variable
+
+my_name = input("Cual es tu nombre? ")
+
+print ("Hola mi nombre es ", my_name , " y tengo " , my_age , " años.")
+
+#imprime el tipo de variale que es, string, int, etc
+print(type(my_name))
+
---
+++ 02.py
@@ -0,0 +1,12 @@
+my_name = "Joan"
+my_age = 32
+my_lastname = "Solé"
+
+completo = "Hola mi nombre es " + my_name + " y mi apellido es " + my_lastname
+print(completo)
+
+completito = "Hola mi nombre es {} y mi apellido es {}".format(my_name, my_lastname)
+print(completito)
+
+otraversion = f"Hola mi nombre es {my_name} {my_lastname} y tengo {my_age}"
+print(otraversion)
