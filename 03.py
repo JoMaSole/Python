def5376d5d98b489f523ebe3bddffe7552e845ce
JoMaSole <joan.m.sole@hotmail.com>
2023-02-02 17:53:09 -0300

    Update 03.py

---
+++ 03.py
@@ -0,0 +1,25 @@
+name = "Joan"
+print(type(name))
+
+name = 12
+print(type(name))
+
+age = 32
+print("Mi edad es " + str(age))
+# la funcion f (format) hace el cambio de tipo de variable automaticamente
+print(f"Mi edad es {age}")
+
+age = input("Escribe tu edad a continuacion: ")
+age = int(age)
+print(f"Tu edad en 10 años sera {age + 10}")
+
+
+# operadores aritmeticos
+
+# (+) Suma
+# (-) Resta
+# (*) Multiplicación
+# (/) División
+# (%) Modulo o Residuo
+# (//) Division con valor entero
+# (**) Exponenciación
