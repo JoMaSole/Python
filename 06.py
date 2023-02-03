d3f0355dec815b0bcb0f7d50902bf8ba30fb6bbd
JoMaSole <105064348+JoMaSole@users.noreply.github.com>
2023-02-03 15:48:16 -0300

    Update 06.py

--- 06.py
+++ 06.py
@@ -1,11 +1,31 @@
 test = "Ya sabe programar en Python"
 #print('JavaScript' in test)
 #print('sabe' in test)
-ingreso = input("ingrese un lenguaje de programacion que conozca: ")
+#ingreso = input("ingrese un lenguaje de programacion que conozca: ")
 
-if (ingreso in test):
-    print("Yo tambien lo conozco, es muy bueno")
-else:
-    print("No lo conozco")
+# if (ingreso in test):
+#     print("Yo tambien lo conozco, es muy bueno")
+# else:
+#     print("No lo conozco")
 
 
+size = len('Garfield')
+print(size)
+
+size2 = len(test)
+print(size2)
+
+print(test, test.upper())
+print(test.lower())
+print(test.count('a'))
+
+print(test.swapcase())
+print(test.startswith('Ya'))
+print(test.endswith('on'))
+print(test.replace('Python', 'PHP'))
+
+text = 'este es un titulo'
+print(text.capitalize())
+print(text.title())
+print(text.isdigit())
+print("159".isdigit())
