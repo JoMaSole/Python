41c4c9a933653578b183599972e7bce8c8a9e15a
JoMaSole <105064348+JoMaSole@users.noreply.github.com>
2023-02-03 16:49:48 -0300

    Update 07.py

--- 07.py
+++ 07.py
@@ -4,5 +4,9 @@ size = len(text)
 print(text[size - 1])
 print(text[-2])
 
-print(text[8:17])
-print(text[:10])
+print(text[8:17])#imprime esa interseccion
+print(text[:10])#desde el principio al 10
+print(text[5:])#desde la posicion 5 al final
+print(text[:])#va del principio al fin
+print(text[5:17:2]) #el tercero indica los saltos
+print(text[::3])#el tercero indica los saltos
