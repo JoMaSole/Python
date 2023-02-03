35aadb9d127bf291d808125275951aff77b4c3ea
JoMaSole <105064348+JoMaSole@users.noreply.github.com>
2023-02-03 15:00:08 -0300

    Update 06.py

---
+++ 06.py
@@ -0,0 +1,11 @@
+test = "Ya sabe programar en Python"
+#print('JavaScript' in test)
+#print('sabe' in test)
+ingreso = input("ingrese un lenguaje de programacion que conozca: ")
+
+if (ingreso in test):
+    print("Yo tambien lo conozco, es muy bueno")
+else:
+    print("No lo conozco")
+
+
